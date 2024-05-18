a4901915b06c0acf7f6e2a18f5e2494cd8a693c4
Arpita Deb <arpitadeb1990@gmail.com>
2024-05-18 15:18:25 -0600

    getting bucket from gcp storage

--- server/service/storage.js
+++ server/service/storage.js
@@ -4,13 +4,13 @@ const path = require('path');
 const { Storage } = require('@google-cloud/storage');
 
 const serviceAccountPath = path.join(__dirname, '../credentials/sharencare-422805-bf2ec8fd49b2.json');
-const serviceAccount = fs.readFileSync(serviceAccountPath, 'utf8');
-const encodedServiceAccount = Buffer.from(serviceAccount).toString('base64');
+const encodeserviceAccount = fs.readFileSync(serviceAccountPath, 'utf8');
+const encodedServiceAccount = Buffer.from(encodeserviceAccount).toString('base64');
 
-console.log(encodedServiceAccount);
+console.log("servc", encodedServiceAccount);
 
-// const serviceAccount = JSON.parse(Buffer.from(process.env.GOOGLE_CLOUD_SERVICE_ACCOUNT, 'base64').toString('utf8'));
-
+const serviceAccount = JSON.parse(Buffer.from(process.env.GOOGLE_CLOUD_SERVICE_ACCOUNT, 'base64').toString('utf8'));
+console.log("line13", serviceAccount);
 const storage = new Storage({
   projectId: serviceAccount.project_id,
   credentials: serviceAccount
